ad78e85c6e6ef0183c2da01d8ed04391cacfa652
beast <admin@beast.com>
2021-01-23 15:29:49 +0300

    Add guess.go

--- guess.go
+++ guess.go
@@ -18,24 +18,35 @@ func main() {
 	target := rand.Intn(100) + 1
 	fmt.Println("I've chosen a random number between 1 and 100.")
 	fmt.Println("Can you guess it?")
-	fmt.Println(target)
+	//fmt.Println(target)
 
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Make a guess: ")
-	input, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
+	success := false
+	for i := 0; i < 10; i++ {
+		fmt.Println("You have ", 10-i, "guesses left.")
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Make a guess: ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		input = strings.TrimSpace(input)
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if guess < target {
+			fmt.Println("Oops. Your guess was LOW.")
+		} else if guess > target {
+			fmt.Println("Oops. Your guess was HIGH.")
+		} else {
+			success = true
+			fmt.Println("Good job! You guessed it!")
+			break
+		}
 	}
-
-	input = strings.TrimSpace(input)
-	guess, err := strconv.Atoi(input)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if guess < target {
-		fmt.Println("Oops. Your guess was LOW.")
-	} else if guess > target {
-		fmt.Println("Oops. Your guess was HIGH.")
+	if success == false { // !success
+		fmt.Println("Sorry. You didn’t guess my number. It was: ", target)
 	}
 }
