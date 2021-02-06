b4780ed2845979d13b3e153083a919a0df844a22
beast <admin@beast.com>
2021-02-06 23:02:03 +0300

    Add new /datafile

---
+++ datafile/strings.go
@@ -0,0 +1,29 @@
+// Package datafile предназанчен для чтения данных из файлов.
+package datafile
+
+import (
+	"bufio"
+	"os"
+)
+
+// GetStrings читает значение string из каждой строки файла.
+func GetStrings(fileName string) ([]string, error) {
+	var lines []string
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+	err = file.Close()
+	if err != nil {
+		return nil, err
+	}
+	if scanner.Err() != nil {
+		return nil, scanner.Err()
+	}
+	return lines, nil
+}
