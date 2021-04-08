c75b940dd9c95b75b5aa90ecb3f5f4a7120896a9
beast <admin@beast.ru>
2021-04-08 14:41:49 +0300

    Add all guestbook

--- guestbook/guestbook.go
+++ guestbook/guestbook.go
@@ -9,6 +9,11 @@ import (
 	"os"
 )
 
+type Guestbook struct {
+	SignatureCount int
+	Signatures     []string
+}
+
 func check(err error) {
 	if err != nil {
 		log.Fatal(err)
@@ -16,14 +21,39 @@ func check(err error) {
 }
 func viewHandler(writer http.ResponseWriter, request *http.Request) {
 	signatures := getStrings("signatures.txt")
-	fmt.Printf("%#v\n", signatures)
 	html, err := template.ParseFiles("view.html")
+	check(err)
+	guestbook := Guestbook{
+		SignatureCount: len(signatures),
+		Signatures:     signatures,
+	}
+	err = html.Execute(writer, guestbook)
+	check(err)
+}
+
+func newHandler(writer http.ResponseWriter, request *http.Request) {
+	html, err := template.ParseFiles("new.html")
 	check(err)
 	err = html.Execute(writer, nil)
 	check(err)
 }
+
+func createHandler(writer http.ResponseWriter, request *http.Request) {
+	signature := request.FormValue("signature")
+	options := os.O_WRONLY | os.O_APPEND | os.O_CREATE
+	file, err := os.OpenFile("signatures.txt", options, os.FileMode(0600))
+	check(err)
+	_, err = fmt.Fprintln(file, signature)
+	check(err)
+	err = file.Close()
+	check(err)
+	http.Redirect(writer, request, "/guestbook", http.StatusFound)
+}
+
 func main() {
 	http.HandleFunc("/guestbook", viewHandler)
+	http.HandleFunc("/guestbook/new", newHandler)
+	http.HandleFunc("/guestbook/create", createHandler)
 	err := http.ListenAndServe("localhost:8080", nil)
 	log.Fatal(err)
 }
