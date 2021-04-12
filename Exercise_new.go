e5e567698e3a4af4665ad7fd925a06df8fd59da2
beast <admin@beast.ru>
2021-04-12 17:12:48 +0300

    Add new Exercise

---
+++ Exercise_new.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func FizzBuzz(n int) {
+	for i := 0; i < n; i++ {
+		if (i+1)%3 == 0 && (i+1)%5 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if (i+1)%3 == 0 {
+			fmt.Println("Fizz")
+		} else if (i+1)%5 == 0 {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(i + 1)
+		}
+	}
+}
+
+func main() {
+	FizzBuzz(100)
+	fmt.Println("\nPlease enter \"q\" to exit")
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		if scanner.Text() == "q" {
+			break
+		}
+	}
+	if scanner.Err() != nil {
+		log.Fatal()
+	}
+}
